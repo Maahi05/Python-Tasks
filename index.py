1d01ca7856b8eb44981ee56de009037fb3b4f6fd
Maahi05 <adusumallimahi@gmail.com>
2026-06-17 17:47:23 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,76 @@
+#different data types int, float, str, bool
+#primitive data types
+#Integer 
+print("About Inetger")
+a = 10
+print(a)
+print(type(a))
+print(id(a))
+#Integer can store 0's
+int1 = 0
+print(int1)
+#Integer can store octal values
+octa = 0o123 #8**0*3+8**1*2+8**2*1
+print(octa)
+#Integer can store hexa values
+hexa = 0x123 #16**0*3+16**1*2+16**2*1
+print(hexa)
+#About Float it is used to store decimal values
+print("About Float")
+b = 4.7
+print(b)
+print(type(b))
+print(id(b))
+flo = 25/100
+print(type(flo))
+fl = 2e2 # 2*(10**2) = 200
+print(fl)
+#About Boolean it will be either true or false
+print("About Boolean")
+bo = bool()
+print(type(bo))
+c = True
+print(c)
+print(id(c))
+print(type(c))
+numb = 200
+numbe = 100
+res = numbe>numb
+print(res)
+#String Sequence of characters represent with quotes
+print("About String")
+stri = str()
+print(type(stri))
+strin = ""
+print(stri)
+d = "Maahi"
+print(d)
+print(type(d))
+print(id(d))
+str1 = "Maahi"
+str2 = "Adusumalli"
+print(str1+' '+str2)
+# name, age, height, employment status
+name = "Maahi"
+age = 22
+height = 5.11
+employement_status = False
+print("My name is:",name)
+print("My age is:",age)
+print("My height is:",height)
+print("My employement_status is:",employement_status)
+# basic arithmetic operations 
+print("About arithmetic operations")
+num1 = 10
+num2 = 30
+result = num1+num2
+print(result)
+num3 = 10.5
+num4 = 20.5
+result1 = num4-num3
+print(result1)
+# the id() function 
+number1 = 10
+number2 = 10
+print(id(number1))
+print(id(number2))
